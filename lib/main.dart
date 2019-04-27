6d7b46180ca697f21c948fd138feea3a80a05460
Harshit Diyora <harshitdiyoradd@gmail.com>
2019-04-27 17:26:57 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Chat App",
+      title: "Chat App with git",
       home: new HomePage(),
     );
   }
